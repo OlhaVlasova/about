11726c0fda214e5de10f25313909c98defacdd32
Olga Vlasova <olga.vlasova@getta.io>
2023-08-31 20:59:16 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,67 @@
 $(document).ready(function() {
 
 
-    langParse("../json/en.json")
+    changeMenu();
+    checkLanguage();
+    
+    function changeMenu() {
+        $("#mobile-nav").css("display", "none");
+
+        $(window).on("scroll", function() {
+            scrollProgress();
+            $(".checked-lang--box").removeClass("open");
+            if(window.scrollY > 25) {
+                $("#header").addClass("scroll");
+            } else {
+                $("#header").removeClass("scroll");
+                $("#header").removeClass("open");
+            }
+        });
+
+        $("#burger").on("click", function() {
+            $("#mobile-nav").css("display", "flex");
+            $("#header").toggleClass("open");
+            
+        });
+    }
+
+    function scrollProgress() {
+        // main function
+        var scrollAmount = $(window).scrollTop();
+        var documentHeight = $(document).height();
+        var windowHeight = $(window).height();
+        var scrollPercent = (scrollAmount / (documentHeight - windowHeight)) * 100;
+        var round = Math.round(scrollPercent);
+        
+        // horizontal scroll bar
+        $(".scrollBar").css("width", scrollPercent + "%");
+    
+    }
+
+
+    function checkLanguage() {
+
+        let value = $("#checked-lang--main").attr("value");
+        let langUrl = "../json/" + value + ".json";
+        $("#checked-lang--main").attr("value", value).text(value);
+        $("#"+value).addClass("none");
+
+        langParse(langUrl);
+
+        $("#checked-lang--main").on("click", function() {
+            $(".checked-lang--box").toggleClass("open");
+        });
+
+        $(".checked-lang--key").on("click", function() {
+            value = $(this).attr("value");
+            langUrl = "../json/" + value + ".json";
+            $(".checked-lang--box").removeClass("open");
+            $("#checked-lang--main").attr("value", value).text(value);
+            $(".checked-lang--key").removeClass("none");
+            $("#"+value).addClass("none");
+            langParse(langUrl);
+        });
+    }
 
     function langParse(url) {
         var data = (function () {
@@ -38,12 +98,7 @@ $(document).ready(function() {
                 $(this).html(text);
             });
         }
-
-        
-        
         translateLg(data);
-
-
     }
 
 
