40dd42eb44829c2a3a0543d2e87bbf21fdd5d828
Olga Vlasova <olga.vlasova@getta.io>
2023-09-03 15:03:28 +0200

    update site

--- js/index.js
+++ js/index.js
@@ -3,20 +3,31 @@ $(document).ready(function() {
     fadeAnimation();
     changeMenu();
     checkLanguage();
+
+    
     
     
     function changeMenu() {
         $("#mobile-nav").css("display", "none");
 
         $(window).on("scroll", function() {
+            let mediaQuery = window.matchMedia('(min-width: 1140px)').matches;
             scrollProgress();
             $(".checked-lang--box").removeClass("open");
-            if(window.scrollY > 100) {
+
+            if(!mediaQuery) {
                 $("#header").addClass("scroll");
+                $("#mobile-nav a").on("click", function() {
+                    $("#header").removeClass("open");
+                });
             } else {
-                $("#header").removeClass("scroll");
-                $("#header").removeClass("open");
-            }
+                if(window.scrollY > 100) {
+                    $("#header").addClass("scroll");
+                } else {
+                    $("#header").removeClass("scroll");
+                    $("#header").removeClass("open");
+                }
+            }            
         });
 
         $("#burger").on("click", function() {
@@ -24,6 +35,43 @@ $(document).ready(function() {
             $("#header").toggleClass("open");
             
         });
+
+        document.addEventListener('touchstart', handleTouchStart, false);
+        document.addEventListener('touchmove', handleTouchMove, false);
+        var xDown = null;
+        var yDown = null;
+        
+        function getTouches(evt) {
+            return evt.touches
+        }
+        
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];
+            xDown = firstTouch.clientX;
+            yDown = firstTouch.clientY;
+        };
+        
+        function handleTouchMove(evt) {
+            if(!document.getElementById("header").classList.contains("open")) return;
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+        
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+        
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+        
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+                if ( xDiff > 0 ) {
+                    document.getElementById("header").classList.remove("open");
+                    document.querySelector("body").classList.remove("noScroll");
+                }        
+            }
+            xDown = null;
+            yDown = null;
+        }
     }
 
     function scrollProgress() {
@@ -102,9 +150,14 @@ $(document).ready(function() {
     }
 
     function fadeAnimation() {
-        const screenHeight = window.screen.height;
-        const screenHeightFade = screenHeight*0.8;
-        const heightOutAnim = 20;
+        let screenHeight = window.screen.height;
+        let screenHeightFade = screenHeight*0.8;
+        let heightOutAnim = 15;
+
+        if(window.matchMedia('(min-width: 1140px)').matches) {
+            screenHeightFade = screenHeight*.97;
+            heightOutAnim = 10;
+        }
 
         $(window).on("scroll", function() {
             fadeInAnimation();
